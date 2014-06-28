302322e0ddfe778e158d1492fe56aeb1220dbe1a
Ernesto Favela <odissey@gmail.com>
2014-06-28 02:32:01 -0500

    Primer Commit
    Primer Commit

---
+++ src/com/example/explicitintentexample/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.explicitintentexample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	
+	static final int REQUEST_IMAGE_CAPTURE = 1;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+	public void iniciarCamara(View v)
+	{
+		 Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+		        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+		    }
+	}
+	
+	
+}
